84619e92a9c093cefc1781d9bff84ef737b9c977
bubble-grace <grace.cinque@marsden.school.nz>
2022-06-17 08:29:29 +1200

    Sprint 11 with website planning

--- sprint_11/buttons.js
+++ sprint_11/buttons.js
@@ -1,5 +1,9 @@
 /**
  * Interactive Object
+ * @param {number} xStart where the x starts
+ * @param {number} yStart where the y starts
+ * @param {number} xMouse where the mouse ends in x
+ * @param {number} yMouse where the mouse ends in y
  */
 
 class InteractiveObject{
@@ -34,7 +38,7 @@ class InteractiveObject{
     }
     mLeave(){
         // to know were the mouse is
-        console.log("Mouse has left the canvas")
+        console.log("Mouse has left the canvas");
     }
 
 }
@@ -68,22 +72,22 @@ class InteractiveObject{
         this.text = text;
         this.textColour = textColour;
         this.inBounds = false;
-        this.name = InteractiveButton
+        this.name = InteractiveButton;
     }
     update(){
         // find the boundary
         this.inBounds = this.getBoundary(this.x,this.y, this.w, this.h,
-            this.xMouse, this.yMouse)
+            this.xMouse, this.yMouse);
         let fill = this.fill;
         if(this.name.selected ===this){
             // fill the button that is selected
-            fill = this.selected
+            fill = this.selected;
         }else if (this.inBounds){
             // fill with the hover function
-            fill =this.over
+            fill =this.over;
         }
         this.draw(this.x,this.y,this.w,this.h,fill, this.stroke,this.text,
-            this.textColour)
+            this.textColour);
     }
     mClick(){
         if(this.inBounds){
@@ -115,7 +119,7 @@ class InteractiveObject{
         ctx.textAlign = 'center';
         ctx.font = myFont;
         ctx.fillStyle = txtCol;
-        ctx.fillText(txt, x + w/2, y + h/2)
+        ctx.fillText(txt, x + w/2, y + h/2);
     }
 }
 InteractiveButton.selected = "";
@@ -142,11 +146,11 @@ class SizeButton extends InteractiveButton{
     // new buttons for width of lines and smiley faces
     constructor(x, y, w, h, fill, over, selected, stroke,text, textColour, size) {
         super(x, y, w, h, fill, over, selected, stroke,text, textColour);
-        this.name = SizeButton
-        this.size = size
+        this.name = SizeButton;
+        this.size = size;
     }
 }
-SizeButton.selected = ""
+SizeButton.selected = "";
 
 /**
  * Star button for number of points (clickable)
@@ -169,11 +173,11 @@ class StarButton extends InteractiveButton{
     // new buttons for the amount of points on a star
     constructor(x, y, w, h, fill, over, selected, stroke,text, textColour, point) {
         super(x, y, w, h, fill, over, selected, stroke,text, textColour);
-        this.name = StarButton
-        this.point = point
+        this.name = StarButton;
+        this.point = point;
     }
 }
-SizeButton.selected = ""
+SizeButton.selected = "";
 
 /**
  * Colour Swatch (clickable)
@@ -208,19 +212,19 @@ class Swatch extends InteractiveObject {
     update(){
         // find the boundary
         this.inBounds = this.getBoundary(this.x,this.y, this.w, this.h,
-            this.xMouse, this.yMouse)
+            this.xMouse, this.yMouse);
         let stroke = this.stroke;
         if(Swatch.selected === this){
             // change the outside colour of the boxes
-            stroke = this.selected
+            stroke = this.selected;
             // set the fill colour
-            Swatch.colour = this.fill
+            Swatch.colour = this.fill;
         }else if (this.inBounds){
             // for a hover
-            stroke =this.over
+            stroke =this.over;
         }
         this.draw(this.x,this.y,this.w,this.h,this.fill, stroke,
-            this.textColour)
+            this.textColour);
     }
     mClick(){
         if(this.inBounds){
@@ -248,7 +252,7 @@ class Swatch extends InteractiveObject {
     }
 }
 Swatch.selected = "";
-Swatch.colour = colArray[0]
+Swatch.colour = colArray[0];
 
 /**
  * Manager
@@ -264,7 +268,7 @@ Swatch.colour = colArray[0]
 class Manager extends InteractiveObject {
     // this is where everything is called
     constructor() {
-        super()
+        super();
         this.w = 0;
         this.h = 0;
         this.objectSet = [];
@@ -276,7 +280,7 @@ class Manager extends InteractiveObject {
         // is the mouse inside the drawing rectangle
         // set ready to draw as true
         if(this.inBounds){
-            this.readytodraw = true
+            this.readytodraw = true;
         }
     }
 
@@ -284,7 +288,7 @@ class Manager extends InteractiveObject {
     mUp(e) {
         super.mUp(e);
         let name = InteractiveButton.selected.text;
-        console.log(name)
+        console.log(name);
         if (this.readytodraw) {
             if (name === "Ellipse") {
                 // Ellipse
@@ -322,35 +326,35 @@ class Manager extends InteractiveObject {
         }
 
     update(){
-        ctx.save()
+        ctx.save();
         // drawing space
-        backRect(rect_start_x,rect_start_y, rect_width, rect_height,"rgb(230,230,230)")
-        ctx.clip()
+        backRect(rect_start_x,rect_start_y, rect_width, rect_height,"rgb(230,230,230)");
+        ctx.clip();
         // finding width and height
         this.w = this.xMouse - this.xStart;
         this.h = this.yMouse - this.yStart;
         // check the boundary again
         this.inBounds = this.getBoundary(this.xMouse,this.yMouse,rect_start_x,rect_start_y, rect_width, rect_height)
-        console.log(this.inBounds)
+        console.log(this.inBounds);
         // print the shapes
         for (let i = 0; i < this.objectSet.length; i++ && this.inBounds) {
-            this.objectSet[i].update()
+            this.objectSet[i].update();
         }
         if (this.readytodraw) {
-            console.log("mouse is down")
+            console.log("mouse is down");
             this.draw();
         }
         // this is to clip
-        ctx.restore()
+        ctx.restore();
         name = InteractiveButton.selected.text;
         if (name === "Undo") {
             // undo by removing last thing in the list
             this.objectSet.pop();
-            InteractiveButton.selected = ""
+            InteractiveButton.selected = "";
         }
         else if (name == "Clear"){
             // clear by making the list be nothing
-            this.objectSet = []
+            this.objectSet = [];
         }
         else{}
 
@@ -369,13 +373,13 @@ class Manager extends InteractiveObject {
         // rectangle
         if(name === "Rectangle") {
                 // outline box
-                this.drawRect(this.xStart, this.yStart, this.w, this.h)
+                this.drawRect(this.xStart, this.yStart, this.w, this.h);
                 // outline guidelines
-                this.drawLine(this.xStart, this.yStart, this.xStart + this.w, this.yStart + this.h, "rgb(0,0,0)", 2)
-                this.drawLine(this.xStart, this.yStart + this.h, this.xStart + this.w, this.yStart, "rgb(0,0,0)", 2)
-                let radius = Math.abs(this.w) / 30
+                this.drawLine(this.xStart, this.yStart, this.xStart + this.w, this.yStart + this.h, "rgb(0,0,0)", 2);
+                this.drawLine(this.xStart, this.yStart + this.h, this.xStart + this.w, this.yStart, "rgb(0,0,0)", 2);
+                let radius = Math.abs(this.w) / 30;
                 if (radius += 5 > this.h) {
-                    radius = Math.abs(this.h) / 10
+                    radius = Math.abs(this.h) / 10;
                 }
                 this.drawCircle(this.xStart + this.w / 2, this.yStart + this.h / 2, Math.abs(radius), "rgb(0,0,0)");
 
@@ -384,7 +388,7 @@ class Manager extends InteractiveObject {
         // circle
         else if (name === "Ellipse") {
                 // outline box
-                this.drawRect(this.xStart, this.yStart, this.w, this.h)
+                this.drawRect(this.xStart, this.yStart, this.w, this.h);
                 this.basicEllipse(this.xStart + this.w / 2, this.yStart + this.h / 2, Math.abs(this.w / 2), Math.abs(this.h / 2), "rgb(0,0,0)", 2);
 
 
@@ -393,7 +397,7 @@ class Manager extends InteractiveObject {
         // star
         else if (name === "Star") {
             // outline box
-            this.drawRect(this.xStart, this.yStart, this.w, this.h)
+            this.drawRect(this.xStart, this.yStart, this.w, this.h);
             this.basicStar(this.xStart , this.yStart , this.w, this.h,"rgb(0,0,0)", StarButton.selected.point);
 
 
@@ -401,13 +405,13 @@ class Manager extends InteractiveObject {
         // heart
         else if (name === "Heart") {
             // outline box
-            this.drawRect(this.xStart, this.yStart, this.w, this.h)
+            this.drawRect(this.xStart, this.yStart, this.w, this.h);
             this.basicHeart(this.xStart , this.yStart , this.w, this.h,"rgb(0,0,0)");
 
         }
         // line
         else if (name === "Line") {
-            this.drawLine(this.xStart, this.yStart, this.xStart+this.w, this.yStart+this.h, "rgb(0,0,0)", 2 )
+            this.drawLine(this.xStart, this.yStart, this.xStart+this.w, this.yStart+this.h, "rgb(0,0,0)", 2 );
         }
         // smiley face
         else if (name === "Smiley Face") {
@@ -419,18 +423,18 @@ class Manager extends InteractiveObject {
 
         }
         else {
-                console.log()
+                console.log();
             }
 
     }
 
 }
 // to get everything from the init file
-Manager.prototype.drawRect = drawRect
-Manager.prototype.drawLine = drawLine
-Manager.prototype.drawCircle = drawCircle
-Manager.prototype.basicEllipse = basicEllipse
-Manager.prototype.basicStar = basicStar
-Manager.prototype.basicHeart = basicHeart
-Manager.prototype.draw_Smiley_Face = draw_Smiley_Face
+Manager.prototype.drawRect = drawRect;
+Manager.prototype.drawLine = drawLine;
+Manager.prototype.drawCircle = drawCircle;
+Manager.prototype.basicEllipse = basicEllipse;
+Manager.prototype.basicStar = basicStar;
+Manager.prototype.basicHeart = basicHeart;
+Manager.prototype.draw_Smiley_Face = draw_Smiley_Face;
 
